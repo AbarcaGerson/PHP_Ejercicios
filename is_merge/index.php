f6a002c0b46f45aefc0f21fe39a01ebb90b7d4e4
Gerson Abarca <geda.abarca@hotmail.com>
2025-01-12 21:31:54 -0500

    fixed bug

---
+++ is_merge/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>isMerge</title>
+</head>
+<body>
+    <?php
+
+        // Enunciado
+        // At a job interview, you are challenged to write an algorithm to check if a given string, s, can be formed from two other strings, part1 and part2.
+
+        // The restriction is that the characters in part1 and part2 should be in the same order as in s.
+
+        // The interviewer gives you the following example and tells you to figure out the rest from the given test cases.
+
+        // For example:
+
+        // 'codewars' is a merge from 'cdw' and 'oears':
+
+        //     s:  c o d e w a r s   = codewars
+        // part1:  c   d   w         = cdw
+        // part2:    o   e   a r s   = oears
+
+        function isMerge($s, $part1, $part2) {
+            return "hola mundo";
+        }
+
+        // Pruebas:
+
+        $textTest = 'codewars';
+        $part1 = 'code';
+        $part2 = 'wars';
+
+        // print_r(isMerge($textTest, $part1, $part2));
+
+        echo 'Hola mundito';
+
+        // echo "hola mundo";
+        // print_r("hola mundo");
+
+    ?>
+</body>
+</html>
